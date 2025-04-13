92c734ded7810cba55b675da4d062e5a48c3a645
shintar0 <133033959+shintar0@users.noreply.github.com>
2025-04-13 16:58:07 +0900

    🎉 Initial commit

---
+++ src/main/java/io/github/shintar0/money_integrator/MoneyIntegratorApplication.java
@@ -0,0 +1,13 @@
+package io.github.shintar0.money_integrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyIntegratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyIntegratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/shintar0/money_integrator/MoneyIntegratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shintar0.money_integrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyIntegratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
